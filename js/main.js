3915c9eff032e1207b3cbebba56e8120204713f6
Cindy Le <cindee.le@gmail.com>
2023-04-10 23:13:54 +0000

    Add a 'submit' event listener to the form element #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-
 const photoURL = document.getElementById('image-url');
 photoURL.addEventListener('input', function (event) {
   const img = document.querySelector('img');
   img.src = event.target.value;
+});
+
+const submitForm = document.querySelector('form');
+submitForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const title = submitForm.elements.title.value;
+  const image = submitForm.elements.image.value;
+  const notes = submitForm.elements.notes.value;
+  const newObject = {
+    title,
+    image,
+    notes,
+    entryId: data.nextEntryId
+  };
+  data.entries.push(newObject);
+  data.nextEntryId++;
+  const img = document.querySelector('img');
+  img.src = './images/placeholder-image-square.jpg';
+  submitForm.reset();
 });
