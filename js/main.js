e958ffa4278d6adc99197b955dc1b1dc2399c701
Cindy Le <cindee.le@gmail.com>
2023-04-12 18:59:50 +0000

    Add an event listener to the ul in the entries #3

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ submitForm.addEventListener('submit', function (event) {
 });
 
 function renderEntry(entry) {
-
   const ul = document.createElement('ul');
   ul.setAttribute('class', 'is-flex');
 
@@ -61,7 +60,27 @@ function renderEntry(entry) {
 
   const pencil = document.createElement('i');
   pencil.setAttribute('class', 'fa-solid fa-pencil');
+  pencil.setAttribute('data-entry-id', entry.entryId);
+
   title.appendChild(pencil);
+
+  pencil.addEventListener('click', function (event) {
+    const clickedEntryId = event.target.attributes['data-entry-id'].value;
+    // Use the viewSwap function to show the form.
+
+    viewSwap('entry-form');
+
+    for (let i = 0; i < data.entries.length; i++) {
+      //     Find the entry object in the data.entries array whose id and matches the data - entry - id attribute value of the clicked entry
+      if (parseInt(data.entries[i].entryId) === parseInt(clickedEntryId)) {
+        // assigns that entry’s object to the data.editing property.
+        data.editing = data.entries[i];
+      }
+    }
+    // Pre - populate the entry form with the clicked entry's values from the object stored in the data.editing property.
+    populateEntryForm();
+    // Updates the title of the entry - form view to Edit Entry
+  });
 
   const notes = document.createElement('p');
   notes.textContent = entry.notes;
@@ -110,7 +129,24 @@ function viewSwap(view) {
   if (view === 'entry-form') {
     entryForm.style.display = 'block';
     entriesPage.className = 'has-display-none';
+
   }
+}
+
+function populateEntryForm() {
+
+  // select each field img, title, description
+  const titleInput = document.getElementById('title');
+  const imageInput = document.getElementById('image-url');
+  const notesInput = document.getElementById('user-notes');
+
+  // populate each field with the entry's value
+  titleInput.value = data.editing.title;
+  imageInput.value = data.editing.image;
+  notesInput.value = data.editing.notes;
+
+  const editTitle = document.querySelector('.new-entry');
+  editTitle.textContent = 'Edit Entry';
 }
 
 const entriesLink = document.querySelector('.menu-entries');
@@ -120,5 +156,7 @@ entriesLink.addEventListener('click', function (event) {
 
 const newEntry = document.querySelector('.new-entries');
 newEntry.addEventListener('click', function (event) {
+  const editTitle = document.querySelector('.new-entry');
+  editTitle.textContent = 'New Entry';
   viewSwap('entry-form');
 });
