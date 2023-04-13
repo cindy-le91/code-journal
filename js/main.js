ad01aec71858d10ac3d165327b6422fdae21d00f
Cindy Le <cindee.le@gmail.com>
2023-04-13 05:51:09 +0000

    Remove all CSS styling that had no affect or was otherwise unnecessary. Test all functionality again in a private browsing session. Fix all errors. #3

--- js/main.js
+++ js/main.js
@@ -17,23 +17,17 @@ submitForm.addEventListener('submit', function (event) {
     title,
     image,
     notes
-  };// I added this to close the object. Whether a new entry or an edited entry
-  // we will grab values from the form at the time so this applies to both possibilities.
-
-  // entryId: data.nextEntryId // this should only be used for creating new entries so
-  // it goes in data.editing === null
+  };
 
   if (data.editing === null) {
     newObject.entryId = data.nextEntryId;
-    data.nextEntryId++;// I moved this here because we should only increment this value for new entries not edited entries
-    data.entries.unshift(newObject); // this should be moved to the conditional that we aren't working on an edited entry.
-    // We are adding in this case. We don't want to add when we are just editing.
+    data.nextEntryId++;
+    data.entries.unshift(newObject);
     const newEntry = renderEntry(newObject);
     div.prepend(newEntry);
-    // viewSwap('entries'); // this can be moved outside of conditionals because we want it to happen whether it is a new
-    // entry or an edited entry on submit.
+
   } else {
-    newObject.entryId = data.editing.entryId;// this applies the id of the current object being edited and adds it to our object.
+    newObject.entryId = data.editing.entryId;
 
     for (let i = 0; i < data.entries.length; i++) {
       if (data.entries[i].entryId === newObject.entryId) {
@@ -44,48 +38,15 @@ submitForm.addEventListener('submit', function (event) {
 
     updateOriginalEntry(updatedEntry, newObject.entryId);
 
-    viewSwap('entries'); // i moved this outside of any conditionals
-    // data.entries.unshift(newObject);
-    // data.nextEntryId++; // this should only increment when a new object is made not an edited object
+    viewSwap('entries');
     const img = document.querySelector('img'); //
     img.src = './images/placeholder-image-square.jpg';
     submitForm.reset();
-    // const newEntry = renderEntry(newObject);
-    // div.prepend(newEntry); // this is already done in the conditional creating a new object
+
     data.editing = null;
     viewSwap('entries');
   }
 });
-// const submitForm = document.querySelector('form');
-// submitForm.addEventListener('submit', function (event) {
-//   event.preventDefault();
-//   const title = submitForm.elements.title.value;
-//   const image = submitForm.elements.image.value;
-//   const notes = submitForm.elements.notes.value;
-//   const newObject = {
-//     title,
-//     image,
-//     notes,
-//     entryId: data.nextEntryId
-
-//     if (data.editing === null) {
-//   const newEntry = renderEntry(newObject);
-//   div.prepend(newEntry);
-//   viewSwap('entries');
-//     } else {
-
-//     }
-//   };
-//   data.entries.unshift(newObject);
-//   data.nextEntryId++;
-//   const img = document.querySelector('img');
-//   img.src = './images/placeholder-image-square.jpg';
-//   submitForm.reset();
-
-//   const newEntry = renderEntry(newObject);
-//   div.prepend(newEntry);
-//   viewSwap('entries');
-// });
 
 function renderEntry(entry) {
   const ul = document.createElement('ul');
@@ -111,6 +72,7 @@ function renderEntry(entry) {
 
   const textDiv = document.createElement('div');
   textLi.appendChild(textDiv);
+  textDiv.setAttribute('class', 'text-div');
 
   const title = document.createElement('h3');
   title.textContent = entry.title;
@@ -127,9 +89,8 @@ function renderEntry(entry) {
     viewSwap('entry-form');
 
     for (let i = 0; i < data.entries.length; i++) {
-      //     Find the entry object in the data.entries array whose id and matches the data - entry - id attribute value of the clicked entry
       if (parseInt(data.entries[i].entryId) === parseInt(clickedEntryId)) {
-        // assigns that entry’s object to the data.editing property.
+
         data.editing = data.entries[i];
       }
     }
@@ -191,12 +152,10 @@ function viewSwap(view) {
 
 function populateEntryForm() {
 
-  // select each field img, title, description
   const titleInput = document.getElementById('title');
   const imageInput = document.getElementById('image-url');
   const notesInput = document.getElementById('user-notes');
 
-  // populate each field with the entry's value
   titleInput.value = data.editing.title;
   imageInput.value = data.editing.image;
   notesInput.value = data.editing.notes;
@@ -211,7 +170,7 @@ function updateOriginalEntry(entry, entryId) {
     if (entries[i].attributes && parseInt(entries[i].attributes['data-entry-id'].value) === parseInt(entryId)) {
       entries[i].replaceWith(entry);
     }
-  //  }
+
   }
 }
 
